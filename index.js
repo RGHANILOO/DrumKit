e959d19a37948510d67f633888012e80b7cc00e4
“rezan5910” <rezan5910@gmail.com>
2023-06-01 18:55:13 +0100

    refactored the logic and fixed a few spelling mistakes in HTML

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 // todo
-    // listen to keydown/up events
-window.addEventListener('keydown', function(e){
-    // in the event of keydown find the corresponding audio element
-    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-    //in the event of keydown find the corrsponding key
-    const key = document.querySelector(`.key[data-key='${e.keyCode}']`)
-    if (!audio) return;// kills the function if incorrect key is pressed
-    // console.log(audio)
-    audio.currentTime = 0 //resets the board after each keypress
-    audio.play()
-    key.classList.add('playing')
-    
-});
-function removeTransition(e){
-    if (e.propertyName !== 'transform') return // skipping the it doesnt have transform property name
-    //this in this contexct is calling on the key
-    // console.log(this)
-    this.classList.remove('playing');
+// listen to keydown/up events
+window.addEventListener("keydown", handleKeyDown);
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
+function handleKeyDown(event) {
+  const keyCode = event.keyCode;
+  // in the event of keydown find the corresponding audio element
+  const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+  //in the event of keydown find the corrsponding key
+  const key = document.querySelector(`.key[data-key='${keyCode}']`);
+  if (!audio) return; // kills the function if incorrect key is pressed
+  // console.log(audio)
+  audio.currentTime = 0; //resets the board after each keypress
+  audio.play();
+  key.classList.add("playing");
 }
-const keys = document.querySelectorAll('.key');
-keys.forEach(key=> key.addEventListener('transitionend', removeTransition));
+function removeTransition(event) {
+  if (event.propertyName !== "transform") return; // skipping the it doesnt have transform property name
+  //this in this contexct is calling on the key
+  // console.log(this)
+  this.classList.remove("playing");
+}
